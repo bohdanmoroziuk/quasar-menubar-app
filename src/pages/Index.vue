e5aaef37d6bfee1a88c36d483cabe0949fe4d542
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-02-19 12:28:19 +0200

    chore: initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    Home
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+});
+</script>
